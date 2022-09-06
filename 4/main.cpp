8a1e34aec185df56ae39d36f24f01794bf06a205
Oleksii <alexseyb62@gmail.com>
2022-09-06 15:38:55 +0300

    Создал конструктор с параметрами и без параметров в ветке master

--- 4/main.cpp
+++ 4/main.cpp
@@ -7,7 +7,18 @@ class Point
 private:
 	int x, y;
 public:
-	Point();
+	Point()
+	{
+		cout << "Constructor by default";
+		x = 0;
+		y = 0;
+	};
+	Point(int a, int b)
+	{
+		cout << "Constructor by params";
+		x = a;
+		y = b;
+	};
 	~Point();
 
 
